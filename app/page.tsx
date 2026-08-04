64c585d577e2dc000a9e78b7ca68b86c38dced01
Muhammed Haleel <muhammedhaleel21@gmail.com>
2026-08-04 17:47:55 +0530

    fix(page): comment out StatsSection component

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
       <HeroSection />
       <TrustSection />
       <AboutSection />
-      <StatsSection />
+      {/* <StatsSection /> */}
       <RecentEvent />
       <ProgramsSection />
       <StartupsSection />
