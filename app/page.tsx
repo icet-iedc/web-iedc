a472812bcfab8e49d1ac013e174262a5d57a7ac5
Muhammed Haleel <muhammedhaleel21@gmail.com>
2026-08-04 23:58:04 +0530

    feat(AboutSection): add TextPath component for animated marquee text

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       <AboutSection />
       {/* <StatsSection /> */}
       <RecentEvent />
-      <ProgramsSection />
+      {/* <ProgramsSection /> */}
       <StartupsSection />
       <LatestGallery />
       <ExecomSection />
